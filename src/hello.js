1980965f028cbdb4bdff0fb93488ab343d5af7bb
Blaine Schmeisser <blainesch@gmail.com>
2017-01-11 15:42:12 -0800

    Remove root script.

--- src/hello.js
+++ src/hello.js
@@ -1,20 +1,13 @@
 module.exports = (pluginContext) => {
-  return {
-    respondsTo: (query) => {
-      return query.match(/hello.*/)
-    },
-    search: (query, env = {}) => {
-      const name = query.split(/\s+/)[1] || ''
-      const title = name ? `Hello, ${name}!` : 'Hello!'
-      return new Promise((resolve, reject) => {
-        resolve([
-          {
-            icon: 'fa-hand-spock-o',
-            title,
-            subtitle: 'Please type your name!',
-          },
-        ])
-      })
-    },
+  return (name, env = {}) => {
+    return new Promise((resolve, reject) => {
+      resolve([
+        {
+          icon: 'fa-hand-spock-o',
+          title: `Hello, ${name}!`,
+          subtitle: 'Please type your name!',
+        }
+      ])
+    })
   }
 }
